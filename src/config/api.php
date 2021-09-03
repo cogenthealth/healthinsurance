bc95333e6d27b6fdec844a40ca2980d40bef6cc6
Sujan Karki <regolithsjk@gmail.com>
2021-09-03 16:51:14 +0545

    newly created files

---
+++ src/config/api.php
@@ -0,0 +1,13 @@
+<?php
+
+    return [
+        "Patient"=> "/api/api_fhir/Patient/",
+        "Location"=> "/api/api_fhir/Location/",
+        "PractitionerRole"=> "/api/api_fhir/PractitionerRole/",
+        "Practitioner"=> "/api/api_fhir/Practitioner/",
+        "Claim"=> "/api/api_fhir/Claim/",
+        "ClaimResponse"=> "/api/api_fhir/ClaimResponse/",
+        "CommunicationRequest"=> "/api/api_fhir/CommunicationRequest/",
+        "EligibilityRequest"=> "/api/api_fhir/EligibilityRequest/",
+        "Coverage"=> "/api/api_fhir/Coverage/"
+    ];
---
+++ src/config/auth.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'username' => env('HI_USERNAME', ''),
+    'password' => env('HI_PASSWORD', '')
+];
