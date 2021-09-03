d296d7bda4670e6fd1a35d465bb7ab4170749099
Sujan Karki <regolithsjk@gmail.com>
2021-09-03 21:09:18 +0545

    service provider not found fix

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '39fea3c350bfb87846b53ddf4a30dee18cf35483',
+        'name' => 'cogenthealth/healthinsurance',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'cogenthealth/healthinsurance' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '39fea3c350bfb87846b53ddf4a30dee18cf35483',
+            'dev_requirement' => false,
+        ),
+    ),
+);
